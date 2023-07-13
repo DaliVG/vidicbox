986739c4bdc66d4c076887629303894c4225cb72
dali.vidic <daliborka_vg@hotmail.com>
2023-07-13 12:36:56 +0100

    Security - Basic Auth Security

---
+++ src/main/java/com/vidic/vidicbox/configuration/BasicAuthWebSecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.vidic.vidicbox.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class BasicAuthWebSecurityConfiguration
+{
+
+}
