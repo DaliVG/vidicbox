c958f5ae12c8441e7c011bd8f736cc91e3631a5e
dali.vidic <daliborka_vg@hotmail.com>
2023-07-14 10:50:00 +0100

    Security - Users registration

--- src/main/java/com/vidic/vidicbox/controllers/UserController.java
+++ src/main/java/com/vidic/vidicbox/controllers/UserController.java
@@ -3,22 +3,30 @@ package com.vidic.vidicbox.controllers;
 import com.vidic.vidicbox.models.Users;
 import com.vidic.vidicbox.services.UserServices;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
     private final UserServices userService;
 
-    @GetMapping
+    @GetMapping("/users")
     public ResponseEntity<List<Users>> findAllUsers() {
-
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PostMapping("/users/signup")
+    private ResponseEntity createUsers(@RequestBody Users user) {
+        try {
+            userService.saveOrUpdate(user);
+        } catch (Exception exception) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity("New user created with username: " + user.getUsername(), HttpStatus.CREATED);
     }
 }
